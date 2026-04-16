11b4f8ad63312d2f3949b3b9b033da2586868053
Colin Agnew <82244635+colin-agnew@users.noreply.github.com>
2026-04-16 09:04:33 -0400

    update copy for ssh

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -294,3 +294,16 @@ vim.api.nvim_create_autocmd({ "TermEnter", "BufEnter" }, {
     end
   end,
 })
+
+vim.opt.clipboard = "unnamedplus"
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
--- lua/plugins/neorg.lua
+++ lua/plugins/neorg.lua
@@ -1,3 +1,46 @@
+-- lazy.nvim spec
+local M = {
+  "nvim-neorg/neorg",
+  ft = "norg",
+  dependencies = {
+    "luarocks.nvim", -- this is it.
+    -- "nvim-treesitter/nvim-treesitter",
+    -- "nvim-treesitter/nvim-treesitter-textobjects",
+    -- "nvim-cmp",
+    -- "nvim-lua/plenary.nvim",
+  },
+  cmd = "Neorg",
+}
+local modules = {
+  ["core.defaults"] = {},
+  ["core.completion"] = { config = { engine = "nvim-cmp", name = "[Norg]" } },
+  ["core.integrations.nvim-cmp"] = {},
+  ["core.concealer"] = { config = { icon_preset = "diamond" } },
+  ["core.keybinds"] = {
+    -- https://github.com/nvim-neorg/neorg/blob/main/lua/neorg/modules/core/keybinds/keybinds.lua
+    config = {
+      default_keybinds = true,
+      neorg_leader = "<Leader><Leader>",
+    },
+  },
+  ["core.dirman"] = {
+    config = {
+      workspaces = {
+        Notes = "~/Neorg/Notes",
+        Work = "~/Neorg/Work",
+      },
+      default_workspace = "Notes",
+    }
+  },
+}
+M.opts = {
+  load = modules,
+}
+return M
+
+
+
+--[[
 return {
     {
         "nvim-neorg/neorg",
@@ -24,4 +67,4 @@ return {
             vim.wo.conceallevel = 2
         end,
     },
-}
+}--]]
